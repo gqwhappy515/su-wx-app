6e4b87b265d8de64be9c1d033eebc2ec2fbbd57b
gqwhappy515 <444171927@qq.com>
2018-09-27 16:37:33 +0800

    初始化一些文件

---
+++ page/views/home/index.js
@@ -0,0 +1,84 @@
+// page/views/home/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    src: '/images/p1.jpg',
+    pepoles:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    this.setData({
+      pepoles:[{
+        url: '/images/p1.jpg'
+      },
+        {
+          url: '/images/p2.jpg'
+        }, {
+          url: '/images/p3.jpg'
+        },
+        {
+          url: '/images/p4.jpg'
+        }, {
+          url: '/images/p5.jpg'
+        },
+        {
+          url: '/images/p2.jpg'
+        }]
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
